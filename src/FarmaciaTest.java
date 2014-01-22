d3a4acd99938f06f3719ed40d0b57dddfa2de270
Deyvison <deyvison.melo@dce.ufpb.br>
2014-01-22 13:23:10 -0300

    Adicionei o before

--- src/FarmaciaTest.java
+++ src/FarmaciaTest.java
@@ -1,10 +1,16 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class FarmaciaTest {
-
+	Farmacia farmacia;
+	
+	@Before
+	public void iniciar(){
+		farmacia = new Farmacia();
+	}
 	@Test
 	public void test() {
 		fail("Not yet implemented");

aa32d65a40fad34ae5eaa23f9d2d2f6272e6f8b0
rivanildo <rivanildo.silva@dce.ufpb.br>
2014-03-08 11:47:02 -0300

    adicionado uma lista na classe GerenteDeClientes

--- src/br/com/ufpb/ProjetoPOO/Farmacia/GerenteDeClientes.java
+++ src/br/com/ufpb/ProjetoPOO/Farmacia/GerenteDeClientes.java
@@ -1,5 +1,13 @@
 package br.com.ufpb.projetopoo.farmacia;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class GerenteDeClientes {
 
+	private List <Cliente> clientes;
+	
+	public GerenteDeClientes(){
+		clientes = new LinkedList<Cliente>();
+	}
 }
